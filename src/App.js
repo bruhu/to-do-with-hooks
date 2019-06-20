30179e292e63d0e6827cb0974896f6d54c17205b
Bru <annabru.abh@gmail.com>
2019-06-20 17:34:21 +0200

    working on todoform

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ function Todo({ todo, index }) {
   return <div className="todo">{todo.text}</div>;
 }
 
+//third functional component we create, will contain the form
+function TodoForm({addTodo}){
+  // this form will have a state to it, so we use the hook again
+  // value  for the state, setValue for the method that updates the state - useState empty by default
+const [value, setValue] = useState("");
+
+const handleSubmit = e => {
+  e.preventDefault();
+  // prevent to submit with an empty value:
+  
+}
+return(
+  <form onSubmit={handleSubmit}>
+    {/* in setValue we want to send whatever is in that input */}
+<input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}></input>
+  </form>
+)
+}
+
 //this will be the main component
 function App() {
   //our state - with 2 variables (also deconstructing):
@@ -29,7 +48,7 @@ function App() {
 
   return (
     <div className="app">
-      Oh, hi. I am the app div.
+      <h4>Tell me what to do!</h4>
       <div className="todo-list">
         {/* map through the todos in our state */}
         {/* todos comes from the destructuring above - todo will be each todo and representing each iteration */}
