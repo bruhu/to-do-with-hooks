6f0586289cd2bf6e2397c7ec90bb54400c681d09
Bru <annabru.abh@gmail.com>
2019-06-20 16:53:43 +0200

    Turned App.js into a functional component - deconstructed useState - whatever

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//this will be the main component
+function App(){
+//our state - with 2 variables (also deconstructing):
+//setTodos will be the method to update the state
+//initial data will go between brackets of useState
+const [todos, setTodos] = useState([
+  {
+    text: "Learn about React",
+    isCompleted: false
+  },
+  {
+    text: "Meet friend for lunch",
+    isCompleted: false
+  },
+  {
+    text: "Build to-do app ",
+    isCompleted: false
+  }
+])
+
+return(
+  <div className="app">
+     Oh, hi. I am the app div.
+    <div className="todo-list">
+{/* map through the todos in our state */}
+{/* todos comes from the destructuring above - todo will be each todo and representing each iteration */}
+{/* so for each todo we want to output a todo component */}
+{todos.map(todo, index) => (
+
+)}
+
     </div>
-  );
+
+  </div>
+)
 }
 
-export default App;
+export default App; 
